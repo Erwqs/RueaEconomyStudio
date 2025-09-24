71f92d440d0e8adb8240578fe02a172b0dcb8126
Erwqs <179415667+Erwqs@users.noreply.github.com>
2025-09-24 11:56:16 +0700

    backported from main

--- app/guild_storage_stub.go
+++ app/guild_storage_stub.go
@@ -11,3 +11,17 @@ func (gm *EnhancedGuildManager) saveGuildsToWebStorage() {
 func (gm *EnhancedGuildManager) loadGuildsFromWebStorage() {
 	// No-op for non-WASM builds - use file system instead
 }
+
+// saveGuildsToMemory stub for non-WASM platforms (should never be called)
+func saveGuildsToMemory(guilds []EnhancedGuildData) error {
+	// This should never be called on non-WASM platforms
+	// The code checks runtime.GOOS == "js" before calling this
+	panic("saveGuildsToMemory called on non-WASM platform")
+}
+
+// loadGuildsFromMemory stub for non-WASM platforms (should never be called)
+func loadGuildsFromMemory() ([]EnhancedGuildData, error) {
+	// This should never be called on non-WASM platforms
+	// The code checks runtime.GOOS == "js" before calling this
+	panic("loadGuildsFromMemory called on non-WASM platform")
+}
